bad62756f0abe29ce5b5c611f0ef5e9d52d7b472
Kyoung-Rok Jang <kyoungrok.jang@gmail.com>
2019-03-02 19:28:35 +0900

    trivial

--- code/model/run.sh
+++ code/model/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python main.py --input ./data/wiki-news-300d-1M-subword.hdf5 \
+python main.py --input ../data/wiki-news-300d-1M-subword.hdf5 \
 		 --num_epochs 4000 \
 		 --denoising \
 		 --noise 0.2 \
 		 --sparsity 0.85 \
-		 --output ./data/wiki-news-300d-1M-subword-spine.hdf5 \
+		 --output ../data/wiki-news-300d-1M-subword-spine.hdf5 \
 		 --hdim 1000
